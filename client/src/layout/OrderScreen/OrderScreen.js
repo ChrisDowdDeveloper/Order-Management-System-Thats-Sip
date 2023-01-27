a31e7569fdf095005971d4b0378060d78b163ca6
Chris Dowd <91644551+ChrisDowdDeveloper@users.noreply.github.com>
2023-01-27 12:44:57 -0600

    item object created to pass to the api for ordering

--- client/src/layout/OrderScreen/OrderScreen.js
+++ client/src/layout/OrderScreen/OrderScreen.js
@@ -1,10 +1,13 @@
 import React, { useEffect, useState } from "react";
 import "./orderScreen.css";
 import { listItems } from "../../utils/api";
+import ErrorAlert from "../../ErrorAlert";
 
 export default function OrderScreen() {
     const [items, setItems] = useState([]);
-    const [onHand, setOnHand] = useState(0);
+    const [error, setError] = useState([]);
+
+    let order = [];
 
     function loadItems() {
         const abortController = new AbortController();
@@ -15,30 +18,53 @@ export default function OrderScreen() {
 
     useEffect(loadItems, []);
 
-    const itemListSyrup = items.map(item => {
-        if (item.item_type === "Syrups 750mL") {
-            return (
-                <div key={item.item_id}>
+    const handleChange = (event) => {
+        event.preventDefault();
+        let item = {
+            item_name: event.target.name,
+            item_url: event.target.value,
+            item_id: event.target.id,
+        }
+        order.push(item);
+        console.log(order);
+        console.log(order.length)
+    }
+
+    const handleDelete = (event) => {
+        event.preventDefault();
+        let index = order.findIndex(o => o.item_id === event.target.id);
+        let removed = order.splice(index, 1);
+        setError(`${event.target.name} removed`)
+    }
+
+
+    return (
+        <div>
+            <ErrorAlert error={error} />
+            {items.map(item => (
+                <div className="border" key={item.item_id}>
                     <p>{item.item_name}</p>
                     <img src={item.item_jpg} width="15%" alt="the item" />
                     <br />
                     <p>Control</p>
                     <p>{item.item_control}</p>
                     <br />
-                    <p>On Hand</p>
-                    <p>{onHand}</p>
-                </div>
-            )
-        } else {
-            return null;
-        }
-    })
-
-    return (
-        <div>
-            {items.map(item => (
-                <div className="border" key={item.item_id}>
-                    {itemListSyrup}
+                    <button
+                        onClick={handleDelete}
+                        value={item.item_url}
+                        id={item.item_id}
+                        name={item.item_name}
+                    >
+                        Remove from Order
+                    </button>
+                    <button
+                        onClick={handleChange}
+                        value={item.item_url}
+                        id={item.item_id}
+                        name={item.item_name}
+                    >
+                        Add to Order
+                    </button>
                 </div>
             ))}
         </div>
